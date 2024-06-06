35e49dc1255295bfe5cce2e07417608904635cc3
Arnaldo Perez <19396163+arnaldoperez@users.noreply.github.com>
2024-06-06 20:23:41 +0000

    Models

--- src/models.py
+++ src/models.py
@@ -1,39 +1,89 @@
+from datetime import date
 import os
-from sqlalchemy import create_engine,String
-from sqlalchemy.orm import mapped_column, DeclarativeBase, Mapped
+from typing import List
+from sqlalchemy import create_engine, String, Integer, Date, ForeignKey
+from sqlalchemy.orm import mapped_column, DeclarativeBase, Mapped, relationship, sessionmaker
 
 
 class Base(DeclarativeBase):
-  pass
-
-class People(Base):
-  __tablename__="people"
-  id:Mapped[int]=mapped_column(primary_key=True)
-  name: Mapped[str] = mapped_column(String(30))
-
+    pass
 
 
 class Films(Base):
-  __tablename__="films"
-  id:Mapped[int]=mapped_column(primary_key=True)
+    __tablename__ = "films"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return "<Films {}>".format(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
 
 
 class Planets(Base):
-  __tablename__="planets"
-  id:Mapped[int]=mapped_column(primary_key=True)
+    __tablename__ = "planets"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return "<Planets {}>".format(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+
+class People(Base):
+    __tablename__ = "people"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return "<People {}>".format(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
 
 
 class Locations(Base):
-  __tablename__="locations"
-  id:Mapped[int]=mapped_column(primary_key=True)
+    __tablename__ = "locations"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return "<Location {}>".format(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
+
+
+class Character(Base):
+    __tablename__ = "charaters"
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    def __repr__(self):
+        return "<Character {}>".format(self.id)
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
 
 
 # Don't edit the lines bellow here - No edite la lineas abajo de aquí
-
 db_url = os.getenv("DATABASE_URL")
 if db_url is not None:
     db_url = db_url.replace("postgres://", "postgresql://")
 else:
     db_url = "sqlite:////tmp/test.db"
-    
-engine = create_engine(db_url, echo=True)
+
+
+# You can add 'echo=True' as param to the next line and see the sql under the hood
+engine = create_engine(db_url)
+
+Session = sessionmaker(bind=engine)
+db = Session()
